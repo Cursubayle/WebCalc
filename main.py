7082259d1e29f0b873bd20558346fe606e843f81
Nick <79155681+Cursubayle@users.noreply.github.com>
2024-12-07 21:00:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from aiohttp import web
-from jinja2 import Environment, Template, FileSystemLoader
+import aiohttp_session
+from jinja2 import Environment, FileSystemLoader
 import variables
+
+template = Environment(loader=FileSystemLoader('')).get_template('index.html')
 
 async def index(request):
     """отрисовываем главную страницу"""
     context = {'op': 'title'}
-    env = Environment(loader=FileSystemLoader(''))
-    template = env.get_template('index.html')
     return web.Response(text=template.render(context),content_type='text/html')
 
 async def handle_submit(request):
     """обрабатыеваем форму"""
+    session = await aiohttp_session.get_session(request)
+    session.clear()
+    data = await request.post()
+    print(data.get('name'))
+    session['value'] = data.get('name')
+    print(type(session),session.items())
+    
     context = {
         'op':variables.op}
-    env = Environment(loader=FileSystemLoader(''))
-    template = env.get_template('index.html')
     return web.Response(text=template.render(context),content_type='text/html')
 
 app = web.Application()
 app.router.add_get('/',index)
-app.router.add_post('/submit',handle_submit)
+app.router.add_post('/',handle_submit)
+aiohttp_session.setup(app, aiohttp_session.SimpleCookieStorage())
+
 
 if __name__=="__main__":
     web.run_app(app)
